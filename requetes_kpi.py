89b80dd6b0849aefb4a306a647e0e022fda26fdc
Hassan TILKI <hassan.tlk@outlook.fr>
2024-01-03 22:25:11 +0100

    ajout docstring

--- requetes_kpi.py
+++ requetes_kpi.py
@@ -1,6 +1,29 @@
+"""
+Module contenant des fonctions pour récupérer des statistiques sur les données des véhicules, notamment le nombre de véhicules et le prix moyen.
+
+"""
+
 import duckdb
 
 def get_count_car(marques: list, modeles: list, annee_min: int, annee_max: int, km_min: int, km_max: int, boite: list, energie: list, prix_min: int, prix_max: int) -> int:
+    """
+    Récupère le nombre de véhicules correspondant aux critères spécifiés.
+
+    ## Parameters:
+        marques (list): Liste de noms de marques à filtrer. Si vide, aucune restriction par marque.
+        modeles (list): Liste de noms de modèles à filtrer. Si vide, aucune restriction par modèle.
+        annee_min (int): Année minimale des véhicules à inclure dans le filtre.
+        annee_max (int): Année maximale des véhicules à inclure dans le filtre.
+        km_min (int): Kilométrage minimal des véhicules à inclure dans le filtre.
+        km_max (int): Kilométrage maximal des véhicules à inclure dans le filtre.
+        boite (list): Liste de types de boîtes de vitesses à inclure dans le filtre.
+        energie (list): Liste de types d'énergie à inclure dans le filtre.
+        prix_min (int): Prix minimal des véhicules à inclure dans le filtre.
+        prix_max (int): Prix maximal des véhicules à inclure dans le filtre.
+    
+    ## Returns:
+        int: Le nombre de véhicules correspondant aux critères spécifiés. En cas d'erreur, retourne 0.
+    """
     try:           
         if marques == [] and modeles == []:
             return duckdb.sql(
@@ -51,10 +74,27 @@ def get_count_car(marques: list, modeles: list, annee_min: int, annee_max: int, 
                 AND prix between {prix_min} AND {prix_max}
                 """).pl().item()
     except: 
-        return "0"
+        return 0
 
 def get_avg_price(marques: list, modeles: list, annee_min: int, annee_max: int, km_min: int, km_max: int, boite: list, energie: list, prix_min: int, prix_max: int) -> str:
+    """
+    Calcule et retourne le prix moyen des véhicules correspondant aux critères spécifiés.
+
+    ## Parameters:
+        marques (list): Liste de noms de marques à filtrer. Si vide, aucune restriction par marque.
+        modeles (list): Liste de noms de modèles à filtrer. Si vide, aucune restriction par modèle.
+        annee_min (int): Année minimale des véhicules à inclure dans le filtre.
+        annee_max (int): Année maximale des véhicules à inclure dans le filtre.
+        km_min (int): Kilométrage minimal des véhicules à inclure dans le filtre.
+        km_max (int): Kilométrage maximal des véhicules à inclure dans le filtre.
+        boite (list): Liste de types de boîtes de vitesses à inclure dans le filtre.
+        energie (list): Liste de types d'énergie à inclure dans le filtre.
+        prix_min (int): Prix minimal des véhicules à inclure dans le filtre.
+        prix_max (int): Prix maximal des véhicules à inclure dans le filtre.
     
+    ## Returns:
+        str: Le prix moyen des véhicules correspondant aux critères spécifiés, arrondi à deux décimales et suivi du symbole "€". En cas d'erreur, retourne "0 €".
+    """
     try:
         if marques == [] and modeles == []:
             prix_moyen =  duckdb.sql(
@@ -118,10 +158,26 @@ def get_avg_price(marques: list, modeles: list, annee_min: int, annee_max: int, 
 
 
 def calcul_delta(marques: list, modeles: list, annee_min: int, annee_max: int, km_min: int, km_max: int, boite: list, energie: list, prix_min: int, prix_max: int):
+    """
+    Calcule et retourne la différence entre le nombre de véhicules correspondant aux critères spécifiés, selon la combinaison de filtres par marque et modèle.
+
+    ## Parameters:
+        marques (list): Liste de noms de marques à filtrer. Si vide, aucune restriction par marque.
+        modeles (list): Liste de noms de modèles à filtrer. Si vide, aucune restriction par modèle.
+        annee_min (int): Année minimale des véhicules à inclure dans le filtre.
+        annee_max (int): Année maximale des véhicules à inclure dans le filtre.
+        km_min (int): Kilométrage minimal des véhicules à inclure dans le filtre.
+        km_max (int): Kilométrage maximal des véhicules à inclure dans le filtre.
+        boite (list): Liste de types de boîtes de vitesses à inclure dans le filtre.
+        energie (list): Liste de types d'énergie à inclure dans le filtre.
+        prix_min (int): Prix minimal des véhicules à inclure dans le filtre.
+        prix_max (int): Prix maximal des véhicules à inclure dans le filtre.
     
+    ## Returns:
+        int or None: La différence entre le nombre de véhicules correspondant aux critères spécifiés, selon la combinaison de filtres par marque et modèle. En cas d'absence de critères (marques et modèles vides), retourne None.
+    """
     if marques == [] and modeles == []:
         return None
-    
     if marques != [] and modeles == []:
         return (
             duckdb.sql(
@@ -147,7 +203,6 @@ def calcul_delta(marques: list, modeles: list, annee_min: int, annee_max: int, k
                     AND prix between {prix_min} AND {prix_max};
                     """).pl().item()
                     )
-    
     if marques == [] and modeles != []:
         return (
             duckdb.sql(
@@ -173,7 +228,6 @@ def calcul_delta(marques: list, modeles: list, annee_min: int, annee_max: int, k
                     AND prix between {prix_min} AND {prix_max};
                     """).pl().item()
                     )
-
     if marques != [] and modeles != []:
         return (
             duckdb.sql(
